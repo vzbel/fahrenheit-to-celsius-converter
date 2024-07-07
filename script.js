9485d1834764cef4f39d6cda02a4fa1cf7bc97c0
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-07 10:53:24 -0600

    Create event listener for input and implement celsius output functionality

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const fahrenheitInput = document.getElementById("fahrenheit-input");
 // Celsius output field
 const celsiusOutput = document.getElementById("celsius-output");
 
+// Start main event listener
+initializeEventListener();
+
+// Function to initialize the fahrenheit input event listener
+function initializeEventListener() {
+  // Listen for key up
+  fahrenheitInput.addEventListener("keyup", () => {
+    // Pass current input value along for further processing
+    handleFahrenheitInput(fahrenheitInput.value);
+  });
+}
+
+// Handle the fahrenheit input by converting it and changing the celsius output
+function handleFahrenheitInput(fahrenheitValue) {
+  // Convert then update the output with the new value.
+  updateOutput(fahrenheitToCelsius(fahrenheitValue));
+}
+
 // Update celsius output
 function updateOutput(newOutput) {
   celsiusOutput.textContent = newOutput;
