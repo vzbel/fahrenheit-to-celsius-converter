8c3b96ccdf623ef66320de9791ed081afe210af2
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-07 10:40:49 -0600

    Get input and output fields in script

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Fahrenheit form input
+const fahrenheitInput = document.getElementById("fahrenheit-input");
+// Celsius output field
+const celsiusOutput = document.getElementById("celsius-output");
+
+// Convert fahrenheit value to celsius
 function fahrenheitToCelsius(fahrenheitValue) {
   return ((fahrenheitValue - 32) * 5) / 9;
 }
