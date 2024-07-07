5d379b13e2ba71c294ea78d5f6ed408c241f29ed
vzbel <142253332+vzbel@users.noreply.github.com>
2024-07-07 10:46:32 -0600

    Add celsius output change functionality

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const fahrenheitInput = document.getElementById("fahrenheit-input");
 // Celsius output field
 const celsiusOutput = document.getElementById("celsius-output");
 
+// Update celsius output
+function updateOutput(newOutput) {
+  celsiusOutput.textContent = newOutput;
+}
 // Convert fahrenheit value to celsius
 function fahrenheitToCelsius(fahrenheitValue) {
   return ((fahrenheitValue - 32) * 5) / 9;
